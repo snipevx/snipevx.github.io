50a2e275a6daf5637a1f4b926523f5e6770c7c72
snipevx <snipevx@test.com>
2025-12-31 16:27:43 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://snipevx.github.io",
+	site: "https://vxsnipe.xyz",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
